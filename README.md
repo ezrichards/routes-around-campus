c407fe1fbece39ab7b1b3d26f63a7d75269fcbd0
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2022-05-13 18:01:47 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# routes-around-campus
-A simple implementation of Dijkstra's algorithm to find the most efficient routes around campus.
+# Routes Around Campus
+A simple implementation of Dijkstra's algorithm to find the most efficient routes around campus. This was made for a Introduction to Computer Science (CSCI101) Final Project I did during Fall 2021. While an implementation of Dijkstra's algorithm was definitely not expected of us, I wanted to try my hand at implementing an algorithm that can have use in real life.
+
+# Running The Program
+In order to run the program, an example file ("campus.txt") and query (see below) are provided. The program will expect an input file, walking speed (decimal number), and start/end locations (strings).
+
+    python routes_around_campus.py
+    campus.txt
+    3
+    Bradford
+    MZ
+This will result in the following output:
+
+    The most efficient distance to MZ is 0.284mi (1498.0ft)!
+    It would take you approximately 5.674 minutes to walk there.
+    You should take the following path: Bradford -> Int2 -> Int9 -> Int10 -> Int11 -> Int13 -> MZ
+
+(note that "Int" is an abbreviation for "intersection")
+
+The following image shows the Google Maps Directions recommended route.
+![google maps route](./images/google_maps_route.png)
+This image shows an estimation of what the program's route looks like.
+![program route](./images/program_route.png)
+
+# Assumptions & Points of Error
+Assumptions had to be made in order to simplify the project so that it could be done in a reasonable timeframe. The project assumes the end user does the following:
+-   End-users walk on actual paths (i.e. no cuts through commons or other grassy or non-concrete/brick areas).
+-   End-users do not cut through buildings.
+-  The end user is walking and is not using any vehicle.
+
+A few points of error likely occurred as well; Google Earth and human error when measuring distances may have caused some distances to be very slightly over or under their actual value. Elevation gain and the natural landscape are also things hard to account for within an algorithm which is just considering distances.
+# Caveats & Improvements
+Given that this was a project for an introductory computer science course, I didn't go too far into Dijkstra's algorithm. For instance, the nodes on this graph are not bidirectional, so the user can only go from start to end with parent nodes that have child nodes. This project was made with the intention to have "Bradford Hall" as the starting node, as seen in the examples.
+
+Another difficulty when making the project was collecting the graph data. I measured within Google Earth to find distances between locations which is definitely not a good solution. A future improvement to this project could be to use OpenStreetMap or other APIs that have already collected data so that data files can be easily generated and queried.
---
+++ routes_around_campus.py
@@ -0,0 +1,126 @@
+# Ethan Richards
+# "Routes Around Campus"
+# CSCI101 Create Project 
+from queue import PriorityQueue
+
+"""
+Calculate all optimal routes via Dijkstra's Algorithm
+given a graph of each vertex and start location.
+
+The graph must have all of the vertex's respective
+vertices and the weight of the edge in order to get there.
+
+Returns a tuple of a dictionary of all vertices with 
+the optimal distance to get there and a dictionary with
+all previous vertices visited to get there from the start_location.
+"""
+def calculate_routes(graph, start_location):
+    # Distances, stored as distance from source to vertex
+    dist = { start_location:0 }
+
+    # All previous nodes visited
+    prev = {}
+
+    # Create priorityqueue and look through graph vertices
+    pqueue = PriorityQueue()
+    for vertex in graph:
+        if vertex != start_location:
+            # Set defaults - if vertex isn't starting location, it's infinity distance away
+            dist.update({ vertex:1000000000 })
+            prev.update({ vertex:None })
+
+        pqueue.put((vertex, dist[vertex]))
+
+    # While priorityqueue isn't empty
+    while not pqueue.empty():
+        # Get the current best vertex
+        current = pqueue.get()
+        current_node, current_weight = current
+
+        # For each adjacent vertex
+        for node, weight in graph[current_node]:
+            path = dist[current_node] + weight
+
+            # If new optimal path is less than the previous distance stored, replace it!
+            if path < dist[node]:
+                dist[node] = path
+                prev[node] = current_node
+                pqueue.put((node, path))
+
+    return (dist, prev)
+
+# Get the inputted data!
+map_file = str(input("MAP FILE> "))
+walking_speed = float(input("WALKING SPEED> "))
+start_location = input("START LOCATION> ") 
+end_location = input("END LOCATION> ")
+
+# Unpack file and assemble graph
+graph = {}
+file = open(map_file, 'r')
+
+for line in file:
+    vertices = line.split()
+
+    # If vertex has no adjacent vertices, still add it
+    if vertices[1] == '0':
+        graph.update({vertices[0]:{}})
+    else:
+        adjacent_array = []
+        for i in range(1, len(line.split())):
+            adjacent = line.split()[i].split(",")
+
+            vertex = adjacent[0]
+            weight = adjacent[1]
+
+            # Build array of adjacent vertices and their weights
+            if graph.get(vertices[0]) != None:
+                for element in graph.get(vertices[0]):
+                    if element not in adjacent_array:
+                        adjacent_array.append(element)
+
+            adjacent_array.append((vertex, int(weight)))
+
+            # Update graph!
+            graph.update({vertices[0]:adjacent_array})
+
+file.close()
+
+distances, previous_nodes = calculate_routes(graph, start_location)
+
+for vertex in distances:
+    if end_location not in distances:
+        print("Can't find the specified end location! Make sure your capitalization and spelling is correct.")
+        break
+    
+    if vertex == end_location:
+        best_length_ft = float(distances[vertex])
+
+        # Divide by 5280 to get miles!
+        best_length = float(best_length_ft / 5280)
+
+        # Minutes it takes to get there = (best length / walking_speed) * 60
+        minutes = float(best_length / walking_speed) * 60
+
+        # Store nodes in path array to later display
+        path = []
+        current_node = end_location
+
+        while current_node != start_location:
+            current_node = previous_nodes.get(current_node)
+            path.append(current_node)
+
+        path.reverse()
+        path.append(end_location)
+
+        # Make display string
+        path_string = ""
+        for i in range(len(path)):
+            if i + 1 >= len(path):
+                path_string += str(path[i])
+            else:
+                path_string += str(path[i]) + " -> "
+
+        print(f"The most efficient distance to {end_location} is {round(best_length, 3)}mi ({best_length_ft}ft)!")
+        print(f"It would take you approximately {round(minutes, 3)} minutes to walk there.")
+        print(f"You should take the following path: {path_string}")
